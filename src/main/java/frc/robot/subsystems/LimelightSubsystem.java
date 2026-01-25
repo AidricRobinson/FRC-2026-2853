e0248ecc3ed098b7d82ba97d51414ed6691c20d4
AidricRobinson <Scarecrow13785@gmail.com>
2026-01-24 15:57:53 -1000

    aligmnet command added - not fully finished

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -1,7 +1,50 @@
 package frc.robot.subsystems;
 
+
+import java.nio.channels.Pipe;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
-    
+    private NetworkTable networkTable;
+
+    public enum Pipeline {
+        DRIVER_VIEW(0), APRILTAG(1);
+        private int PipelineID;
+
+        private Pipeline(int PipelineID) {
+            this.PipelineID = PipelineID;
+        }
+    }
+
+    public LimelightSubsystem () {
+        networkTable = NetworkTableInstance.getDefault().getTable("limelight");
+        setPipeline(Pipeline.DRIVER_VIEW);
+    }
+
+    public void setPipeline(Pipeline pipeline) {
+        networkTable.getEntry("pipeline").setValue(pipeline.PipelineID);
+    }
+
+    public double getTx() {
+        return networkTable.getEntry("tx").getDouble(0.0); // will be changed for kitbot limelight placement
+    }
+
+    public double getTy() {
+        return networkTable.getEntry("ty").getDouble(0.0);
+    }
+
+    public double getTa (){
+        return networkTable.getEntry("ta").getDouble(0.0);
+    } 
+    @Override
+    public void periodic(){
+        SmartDashboard.putNumber("tx", getTx());
+        SmartDashboard.putNumber("ty", getTy());
+        SmartDashboard.putNumber("ta", getTa());
+        SmartDashboard.updateValues();
+    }
 }
